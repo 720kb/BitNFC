54c887ec03dc03f179027f28b6898c4fc47a5135
Francesco C <francescocanessa@quillcontent.com>
2015-05-05 15:33:44 +0100

    nfc basic works

--- www/js/app.js
+++ www/js/app.js
@@ -18,6 +18,32 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
+
+    var nfc = window.nfc
+
+    // Read NDEF formatted NFC Tags
+    nfc.addNdefListener (
+        function (nfcEvent) {
+            var tag = nfcEvent.tag,
+                ndefMessage = tag.ndefMessage;
+
+            // dump the raw json of the message
+            // note: real code will need to decode
+            // the payload from each record
+            alert(JSON.stringify(ndefMessage));
+
+            // assuming the first record in the message has
+            // a payload that can be converted to a string.
+            alert(nfc.bytesToString(ndefMessage[0].payload).substring(3));
+        },
+        function () { // success callback
+            alert("Waiting for NDEF tag");
+        },
+        function (error) { // error callback
+            alert("Error adding NDEF listener " + JSON.stringify(error));
+        }
+    );
+
   });
 })
 
