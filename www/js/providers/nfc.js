c9a782c939723ee39a23673ea87df55ee3be197f
Francesco C <francescocanessa@quillcontent.com>
2015-05-08 00:59:56 +0100

    add readme and interval and logs -- sorry dario

--- www/js/providers/nfc.js
+++ www/js/providers/nfc.js
@@ -9,39 +9,48 @@
     return {
       '$get': ['$window', '$rootScope',
         function providerConstructor($window, $rootScope) {
+          console.log("constructor")
 
         var onNFCInitSuccess = function onNFCInitSuccess() {
 
+            console.log("status ok")
             $rootScope.$emit('nfc:status-ok');
           }
           , onNFCInitError = function onNFCInitError(error) {
 
+            console.log("status ko")
             $rootScope.$emit('nfc:status-ko', {
               'error': error
             });
           }
           , onNFCEvent = function onNFCEvent(nfcEvent) {
+            console.log("status event!!!")
             var tag = nfcEvent.tag
-              , ndefMessage = tag.ndefMessage;
+              , ndefMessage = tag.ndefMessage
+              , message;
 
-            // dump the raw json of the message
-            // note: real code will need to decode
-            // the payload from each record
-            $window.alert(JSON.stringify(ndefMessage));
-
+            message = $window.nfc.bytesToString(ndefMessage[0].payload).substring(3)
+            console.log("message: The NFC tag contains: '" + message + "'")
             // assuminahg the first record in the message has
             // a payload that can be converted to a string.
-            $window.alert($window.nfc.bytesToString(ndefMessage[0].payload).substring(3));
+            $window.alert(message);
           };
 
-        if ($window.nfc) {
 
-          // Read NDEF formatted NFC Tags
-          $window.nfc.addNdefListener(onNFCEvent, onNFCInitSuccess, onNFCInitError);
-        } else {
+        var time;
 
-          $rootScope.$emit('nfc:in-browser');
-        }
+        // if (this.state.NFCButtonHasBeenPressed) {
+        time = 1000;
+        // } else { // in any other state
+        // time = 3000;
+        // }
+
+        // $rootScope.$emit('nfc:log:martellatona');
+        console.log("martellatona")
+        setInterval(function(){
+          console.log("listener")
+          nfc.addNdefListener(onNFCEvent, onNFCInitSuccess, onNFCInitError);
+        }.bind(this), 1000)
       }]
     };
   }]);
