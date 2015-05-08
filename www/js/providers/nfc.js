9e252ddc243f3b07c72685bda8f5957c41859c11
wouldgo <wouldgo84@gmail.com>
2015-05-08 22:56:01 +0200

    indent and fix on nfc

--- www/js/providers/nfc.js
+++ www/js/providers/nfc.js
@@ -1,4 +1,4 @@
-/*global angular*/
+/*global angular nfc*/
 (function withAngular(angular) {
   'use strict';
 
@@ -15,7 +15,6 @@
             $rootScope.$apply(function doApply(scope) {
 
               scope.$emit('nfc:status-ok');
-              $window.console.log('\r\n\r\nALL OK\r\n\r\n')
             });
           }
           , onNFCInitError = function onNFCInitError(error) {
@@ -32,9 +31,10 @@
             var tag = nfcEvent.tag
               , ndefMessage = tag.ndefMessage
               , message = $window.nfc.bytesToString(ndefMessage[0].payload).substring(3);
-
             $rootScope.$apply(function doApply(scope) {
+
               if (message.indexOf('s:0?body=') >= 0) {
+
                 scope.$emit('nfc:status-message', {
                   'privateKey': message.substr(0, 9)
                 });
@@ -49,8 +49,6 @@
           , registerListeners = function registerListeners() {
 
             nfc.addNdefListener(onNFCEvent, onNFCInitSuccess, onNFCInitError);
-            //nfc.addTagDiscoveredListener(onNFCEvent, onNFCInitSuccess, onNFCInitError);
-            //nfc.addNdefFormatableListener(onNFCEvent, onNFCInitSuccess, onNFCInitError);
           };
 
         return {
