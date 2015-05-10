9499353ee6254444459c4712304adc7ab1fb4906
Francesco (makevoid) <makevoid@gmail.com>
2015-05-10 11:27:21 +0100

    filter to convert units

--- www/js/filters/bitcoin.js
+++ www/js/filters/bitcoin.js
@@ -2,12 +2,31 @@
 (function withAngular(angular, require) {
   'use strict';
 
-  angular.module('BitCoin.filter', [])
+  angular.module('Units.filter', [])
 
-  .filter('BitCoin',[ function () {
+  .filter('Units',[ function () {
 
-    return function (method) {
+    return function convert(amount, unitFrom, unitTo) {
 
+      switch (unitFrom+':'+unitTo) {
+        case 'mbtc:btc':
+          new bitcore.Unit.fromMilis(amount).toBTC();
+        case 'btc:mbtc':
+          // TODO
+        case 'satoshi:mbtc':
+          // TODO
+        case 'mbtc:satoshi':
+          // TODO
+        case 'btc:satoshi':
+          // TODO
+        case 'satoshi:btc':
+          // TODO
+        default:
+          '9999999 [match not found - fix!]';
+      }
     };
   }]);
 }(angular, require));
+
+// {{address | Unites | convert:amount:unitFrom:unitTo }} //=> number
+// {{address | Unites | convert:100:mbtc:btc }} //=> 0.1
