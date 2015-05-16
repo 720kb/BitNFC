89e2abe8cf79081bed75382624974edf11688097
45kb <filippo.oretti@gmail.com>
2015-05-16 13:05:43 +0200

    Refresh balance directive

--- www/js/directives/balance.js
+++ www/js/directives/balance.js
@@ -3,17 +3,25 @@
   'use strict';
 
   angular.module('Balance.directive', [])
-  .directive('BalanceRefresh', ['$filter', function ($filter) {
+  .directive('balanceRefresh', ['$filter', 'BitCoin', function ($filter, BitCoin) {
     return {
       'restrict': 'E',
       'templateUrl': 'views/module/balanceRefresh.html',
-      'scope': true,
       'link': function (scope, element, attr) {
 
-        var oldBalance = attr.balance();
+        var oldBalance = attr.balance || 0;
 
         scope.refreshBalance = function () {
-        	alert('Yo');
+
+          if (!scope.refreshingBalance) {
+
+            scope.refreshingBalance = true;
+            BitCoin.balance().then(function onBalance(balance) {
+
+              scope.balance = balance;
+              scope.refreshingBalance = false;
+            });
+          }
         };
       }
     };
